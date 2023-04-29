b95ce8b65a22c1d9bda43731071363c238b9afdc
PurnaNandhiniBurugupalli <114227492+PurnaNandhiniBurugupalli@users.noreply.github.com>
2023-04-29 22:05:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function login_valid(){
+    // alert("ok")
+     //alert(document.getElementById(".user2").textContent())
+     let d1=d1ocument.getElementById(".user2").value ;
+     let d2=document.getElementById(".pass2").value;
+     if(d1==""||d2==""){
+         alert("Fill all the fields")
+         console.log("hiikkkkkkkkkkkkkkkkk")
+         return false;
+     }
+     else{
+         return true;
+     }
+ }
+  function employe_log(){
+     return true;
+  }
